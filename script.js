ff6c45feca9017cfbd2cd309a6ec2ff92c050444
ShubhenduVerma <shubh01itsme@outlook.com>
2026-02-24 15:02:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Marine Corporate Website Loaded");
+// ===== Scroll Fade Animation =====
+
+const sections = document.querySelectorAll('.fade-section');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+}, {
+    threshold: 0.15
+});
+
+sections.forEach(section => {
+    observer.observe(section);
+});
